1cb0ab03516d6478d49688d58b957651e9fced28
lilith archmachine <lilith.alanis.255@gmail.com>
2023-01-20 17:24:18 -0600

    [[good commit practice goes here]]

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,25 @@ fn main(){
     } + "project_manager";
     */
     let config_path = "project_manager";
-    let config_data = std::fs::read_to_string(config_path).unwrap_or_else(|_|{
+    let mut config_data = std::fs::read_to_string(config_path).unwrap_or_else(|_|{
         if std::fs::File::create(config_path).is_err() {
             println!("could not create config file at {}", config_path);
         }
         String::new()
     });
 
-    println!("data: {}", config_data);
+    while config_data.find("\n\n").is_some(){
+        config_data = config_data.replace("\n\n", "\n");
+    }
+    let lines_split = config_data.split('\n');
+    let mut lines_raw = Vec::<String>::new();
+    for line in lines_split{
+        if line.len() == 0{
+            continue;
+        }
+        lines_raw.push(line.to_string());
+    }
+
+    println!("{:?}", lines_raw);
+    // test push
 }
