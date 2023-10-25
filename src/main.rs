257261ea9a87ee1c96fa271ca43d804037c22df2
Ulises Alanis <ulises.alanis.255@gmail.com>
2023-10-25 17:01:01 -0600

    fixed something

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,16 @@ fn main() -> ProjectResult<()>{
     let manager = manager::get_config();
     for project in manager.projects{
         let p = project::load_project(project.path)?;
-        println!("{p:?}");
+        let p_done = p.get_done();
+        let p_todo = p.get_todo();
+
+        let p_total = p_todo + p_done;
+        let p_done_p = 100. * if p_total != 0. {p_done / p_total} else {0.};
+
+        println!("{:30} {p_done_p:04.2}", p.info.name);
     }
+
+
 
 
     Ok(())
