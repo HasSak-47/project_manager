8d99092d6e57b6728104e338a47e8de002be964c
Ulises Alanis <ulises.alanis.255@gmail.com>
2023-10-24 19:17:47 -0600

    barebones project reader

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,12 +1,2 @@
 pub mod manager;
 pub mod project;
-
-pub mod prelude{
-    pub use super::manager::*;
-    pub use super::project::*;
-}
-
-pub mod prelude2{
-    pub use super::manager;
-    pub use super::project;
-}
