efbf57843afbb55dce80bb58fc9159967a3c6f77
Ulises Alanis <ulises.alanis.255@gmail.com>
2023-10-24 19:28:14 -0600

    barebones project reader

--- src/config/project.rs
+++ src/config/project.rs
@@ -34,17 +34,19 @@ pub struct ProjectToml{
 
 use crate::error::ProjectResult;
 
-fn gen_feature(t: &Table) -> Feature{
+fn gen_feature(t: &Table) -> ProjectResult<Feature>{
     let mut f = Feature::default();
-    f.name = t["name"].as_str().unwrap().to_string();
+    use crate::error::to_res;
+    
+    f.name = to_res(t["name"].as_str())?.to_string();
     f.description = t["name"].as_str().map(str::to_string);
-    f.difficulty = t["difficulty"].as_float().unwrap() as f32;
-    f.priority = t["priority"].as_float().unwrap() as f32;
+    f.difficulty = to_res(t["difficulty"].as_float())? as f32;
+    f.priority = to_res(t["priority"].as_float())? as f32;
 
-    f
+    Ok(f)
 }
 
-fn get_feature(t: &Table) -> Feature{
+fn get_feature(t: &Table) -> ProjectResult<Feature>{
     let mut f = gen_feature(t);
     let subfeature = t.get("subfeature");
     if let None = subfeature{
@@ -55,7 +57,7 @@ fn get_feature(t: &Table) -> Feature{
     match arr{
         Some(subfeats) => {
             for feat in subfeats {
-                f.sub_feature.push(get_feature(feat.as_table().unwrap()))
+                f?.sub_feature.push(get_feature(feat.as_table().unwrap())?)
             }
         },
         None => return f,
@@ -73,10 +75,10 @@ pub fn load_project<S: std::fmt::Display>(path : S) -> ProjectResult<Project>{
 
     project.info = project_toml.project;
     for f in project_toml.todo.unwrap_or(Vec::new()){
-        project.todo.push(get_feature(&f.as_table().unwrap()));
+        project.todo.push(get_feature(&f.as_table().unwrap())?);
     }
     for f in project_toml.done.unwrap_or(Vec::new()){
-        project.done.push(get_feature(&f.as_table().unwrap()));
+        project.done.push(get_feature(&f.as_table().unwrap())?);
     }
 
     Ok(project)
