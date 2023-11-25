fba23eb7cb87ccc3f8b59370f24d5d5eff979f1f
arch machine <lilith.alanis.255@gmail.com>
2023-11-25 15:36:31 -0600

    update system

--- src/error.rs
+++ src/error.rs
@@ -7,7 +7,8 @@ pub enum ProjectError{
     IOError(std::io::Error),
     UTF8Error(std::str::Utf8Error),
     UTF8ErrorString(std::string::FromUtf8Error),
-    TOMLFromStr(toml::de::Error)
+    TOMLFromStr(toml::de::Error),
+    TOMLToStr(toml::ser::Error),
 }
 
 pub type ProjectResult<T> = Result<T, ProjectError>;
@@ -26,6 +27,7 @@ into_error!(std::io::Error, IOError);
 into_error!(std::str::Utf8Error, UTF8Error);
 into_error!(std::string::FromUtf8Error, UTF8ErrorString);
 into_error!(toml::de::Error, TOMLFromStr);
+into_error!(toml::ser::Error, TOMLToStr);
 
 pub fn get_dir(a: fn() -> Option<std::path::PathBuf>) -> ProjectResult<String>{
     use ProjectError as PE;
