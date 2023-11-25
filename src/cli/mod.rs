aed25bd2d9e28ef336884247baa48cebdcd39187
Ulises Alanis <ulises.alanis.255@gmail.com>
2023-11-24 22:31:23 -0600

    fucker

---
+++ src/cli/mod.rs
@@ -0,0 +1,38 @@
+mod daemon;
+use crate::error::ProjectResult;
+
+use crate::Manager;
+use crate::EDITION;
+
+
+pub trait CliUtil{
+    fn run() -> ProjectResult<()>;
+}
+
+pub struct PrintPercentajes();
+pub struct SelectRandomProject();
+
+impl CliUtil for PrintPercentajes{
+    fn run() -> ProjectResult<()> {
+        let manager = Manager::get_config();
+        let mut projects = manager.get_unbroken_projects();
+
+        projects.sort_by(|a,b|{
+            let ac = a.get_completion();
+            let bc = b.get_completion();
+            bc.partial_cmp(&ac).unwrap()
+        });
+
+
+        println!("pm version {}", EDITION);
+        for p in projects{
+            print!("{:20}: {:>7.2}%", p.info.name, p.get_completion() * 100.);
+            if p.info.edition != EDITION {
+                print!(" config out date! '{}'", p.info.edition);
+            }
+            println!();
+        }
+        Ok(())
+    }
+}
+
