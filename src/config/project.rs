b393c97a22719492e4f3fdc0d7cd43d55cbed8b3
Ulises Alanis <ulises.alanis.255@gmail.com>
2023-11-24 22:45:42 -0600

    fixed toml impl

--- src/config/project.rs
+++ src/config/project.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use toml::{Table, value::Array};
+use crate::error::*;
 
 #[derive(Deserialize, Serialize, Debug, Default, Clone)]
 pub struct Feature{
@@ -11,14 +12,6 @@ pub struct Feature{
     pub todo: Vec<Feature>,
 }
 
-#[derive(Debug, Default, Clone)]
-pub struct Project{
-    pub info : ProjectInfo,
-    pub subproj: Vec<Project>,
-    pub todo: Vec<Feature>,
-    pub done: Vec<Feature>,
-}
-
 #[derive(Default, Clone, Deserialize, Serialize, Debug)]
 pub struct ProjectInfo{
     pub name: String,
@@ -26,56 +19,31 @@ pub struct ProjectInfo{
     pub edition: String,
 }
 
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct ProjectToml{
+    pub project: ProjectInfo,
+    pub subproj: Option<Vec<Project>>,
+    pub todo   : Option<Vec<Feature>>,
+    pub done   : Option<Vec<Feature>>,
+}
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct ProjectToml{
-    project : ProjectInfo,
-    subproj : Option<Array>,
-    todo    : Option<Array>, 
-    done    : Option<Array>, 
+pub struct Project{
+    pub project: ProjectInfo,
+    pub subproj: Vec<Project>,
+    pub todo   : Vec<Feature>,
+    pub done   : Vec<Feature>,
 }
 
-use crate::error::ProjectResult;
-
-fn gen_feature(t: &Table) -> ProjectResult<Feature>{
-    let mut f = Feature::default();
-    use crate::error::to_res;
-    
-    f.name = to_res(t["name"].as_str())?.to_string();
-    f.description = t.get("description").map(|x| x.as_str().unwrap_or("").to_string());
-    f.difficulty = to_res(t["difficulty"].as_float())? as f32;
-    f.priority = to_res(t["priority"].as_float())? as f32;
-
-    Ok(f)
-}
-
-fn get_subfeatures(value: &toml::Value) -> ProjectResult<Vec<Feature>>{
-    let mut v = Vec::new();
-    match value.as_array(){
-        Some(feats) => {
-            for feat in feats{
-                v.push(get_feature(feat.as_table().unwrap())?);
-            }
-        },
-        None => {}
-    };
-
-    Ok(v)
-}
-
-fn get_feature(t: &Table) -> ProjectResult<Feature>{
-    let mut f = gen_feature(t)?;
-    let done = t.get("done");
-    let todo = t.get("todo");
-    if let Some(done_feats_val) = done {
-        f.done.append(&mut get_subfeatures(done_feats_val)?);
+impl From<ProjectToml> for Project{
+    fn from(value: ProjectToml) -> Self {
+        Self {
+            project: value.project,
+            subproj: value.subproj.unwrap_or(Vec::new()),
+            todo: value.todo.unwrap_or(Vec::new()),
+            done: value.done.unwrap_or(Vec::new()),
+        }
     }
-
-    if let Some(todo_feats_val) = todo {
-        f.todo.append(&mut get_subfeatures(todo_feats_val)?);
-    }
-
-    Ok(f)
 }
 
 fn load_project<S: AsRef<str>>(path : S) -> ProjectResult<Project>{
@@ -84,24 +52,15 @@ fn load_project<S: AsRef<str>>(path : S) -> ProjectResult<Project>{
     let data = crate::utils::read_file(path)?;
 
     let project_toml : ProjectToml = toml::from_str(std::str::from_utf8(data.as_bytes())?)?;
-    let mut project = Project::default();
 
-    project.info = project_toml.project;
-    for f in project_toml.todo.unwrap_or(Vec::new()){
-        project.todo.push(get_feature(&f.as_table().unwrap())?);
-    }
-    for f in project_toml.done.unwrap_or(Vec::new()){
-        project.done.push(get_feature(&f.as_table().unwrap())?);
-    }
-
-    Ok(project)
+    Ok(project_toml.into())
 }
 
 impl Project {
     #[allow(unused)]
     pub const fn new() -> Self{
         Project{
-            info: ProjectInfo {
+            project: ProjectInfo {
                 name: String::new(),
                 version: String::new(),
                 edition: String::new(),
