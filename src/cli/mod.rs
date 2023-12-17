2a9ab659e5e6c3e7ee0dee4f4796657510eda630
arch machine <lilith.alanis.255@gmail.com>
2023-12-17 00:17:35 -0600

    readme!

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,7 +1,3 @@
-pub mod daemon;
-pub mod general;
-pub mod project;
-
 use crate::error::ProjectResult;
 
 type StStrA = &'static [&'static str];
@@ -9,7 +5,8 @@ type StStrA = &'static [&'static str];
 pub trait CliUtil where
     Self: Sized,
 {
-    const NAMES : StStrA;
-    fn run() -> ProjectResult<()>;
-    fn add_arg(self) -> Self { self }
+    fn run(self) -> ProjectResult<()>;
+    fn add_arg(self, arg: String) -> Self { self }
 }
+
+
