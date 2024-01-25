b78c6d75223bca69600a2c9293380098f538e538
Ulises Alanis <ulises.alanis.255@gmail.com>
2024-01-24 22:46:28 -0600

    did a sin in the print per function lmao

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
 mod cli;
 mod update;
 mod config;
 mod error;
 mod utils;
 
-use cli::run_cli;
+use cli::cli;
 use error::ProjectResult;
 use config::manager::Manager;
-use std::env::args;
 
 #[allow(dead_code)]
 const EDITION : &str = "0.1.0";
 
 fn main() -> ProjectResult<()>{
     Manager::create_data()?;
-    let mut args : Vec<_> = args().collect();
-    args.remove(0);
-    if args[0] == "tui"{
-        
-    }
-    else{
-        run_cli(args)?;
-    }
+    cli()?;
     Ok(())
 }
