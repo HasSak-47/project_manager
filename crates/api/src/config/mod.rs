6a5dc2575a4ab0c882f57198f5dc4220b0b75233
Ulises Alanis <ulises.alanis.255@gmail.com>
2024-02-07 16:36:20 -0600

    adding tests

--- crates/api/src/config/mod.rs
+++ crates/api/src/config/mod.rs
@@ -1,4 +1,8 @@
+/*
+ * Stuff X labled XToml is the representation in the .toml
+ * and X is the inner representation that is more nice to work with 
+ */
+
 pub mod manager;
 pub mod project;
 pub mod default;
-pub mod daemon;
