6a5ce851c84d5b0d591ffbf532f6b4b22093c7da
arch machine <lilith.alanis.255@gmail.com>
2024-04-19 14:57:49 -0600

    v2 replaced v1

--- crates/api/src/config/default.rs
+++
@@ -1,9 +0,0 @@
-
-pub const DEFAULT_MANAGER : &str = include_str!("./default_manager.toml");
-
-pub fn create_project<S: AsRef<str>>(name: S, version: S, edition: S) -> String {
-    let name = name.as_ref();
-    let version = version.as_ref();
-    let edition = edition.as_ref();
-    format!(include_str!("./default_project.toml"), name, version, edition)
-}
