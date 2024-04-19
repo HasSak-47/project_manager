a519dfa5a786542edcae915ed1accf5db8516e2a
Ulises Alanis <ulises.alanis.255@gmail.com>
2024-04-19 14:57:49 -0600

    v2 replaced v1

--- crates/api/src/config/mod.rs
+++
@@ -1,8 +0,0 @@
-/*
- * Stuff X labled XToml is the representation in the .toml
- * and X is the inner representation that is more nice to work with 
- */
-
-pub mod manager;
-pub mod project;
-pub mod default;
--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -1,199 +1,104 @@
-use std::collections::HashMap;
+use std::path::{Path, PathBuf};
 
-use config::{project::{Project, ProjectInfo}, manager::{ProjectData, Manager, Location}};
 use anyhow::{Result, anyhow};
+mod manager;
+mod project;
 
-pub use cached_project::*;
-
-pub mod cached_project;
-pub mod v2;
-pub mod config;
-
-pub trait ProjectLoader{
-    fn get_manager(&self) -> Result<String>;
-    fn get_project(&self, location: &Location) -> Result<String>;
-    fn write_manager(&mut self, data: String) -> Result<()>;
-    fn write_project(&mut self, data: String, location: &Location) -> Result<()>;
-    fn ensure_existance(&mut self) -> Result<()>;
+#[derive(Debug, Clone)]
+pub enum Location{
+    Path(PathBuf),
+    Url(String),
 }
+
+impl Default for Location{ fn default() -> Self{ Location::Path(PathBuf::new()) } }
+
+impl Location{
+    pub fn path<P : AsRef<Path>>(path: P) -> Self{ Location::Path(path.as_ref().to_path_buf()) }
+    pub fn url<S: AsRef<str>>(url: S) -> Self{ Location::Url(url.as_ref().to_string()) }
+
+    pub fn get_path(&self) -> Result<&Path>{
+        match self{
+            Location::Path(path) => Ok(path.as_path()),
+            _ => Err(anyhow!("Location is not a path!!")),
+        }
+    }
+
+    pub fn get_url(&self) -> Result<&str>{
+        match self{
+            Location::Url(url) => Ok(url.as_str()),
+            _ => Err(anyhow!("Location is not a url!!")),
+        }
+    }
+}
+use project::*;
+use manager::*;
 
 #[derive(Debug, Default)]
-pub struct ProjectsHandler<Loader: ProjectLoader>{
-    _loader: Loader,
-    _projects : HashMap<String, CachedProject>,
+pub struct Handler{
+    pub project_handler: ProjectHandler,
+    pub manager_handler: ManagerHandler,
+
+    pub manager: Manager,
+    _inited: bool,
 }
 
-impl<Loader> ProjectsHandler<Loader>
-where
-    Loader: ProjectLoader
-{
-    pub fn init(mut loader: Loader) -> Result<Self>{
-        match loader.ensure_existance(){
-            Ok(_) => {},
-            Err(_) => loader.write_manager(
-                toml::to_string(&Manager::default())?
-            )?,
-        };
-        loader.ensure_existance().unwrap();
-        let manager : Manager = loader
-            .get_manager()
-            .and_then(|p| Ok(toml::from_str(p.as_str())))??;
+impl Handler{
+    pub fn new() -> Self{ Self::default() }
 
-        let _projects = manager.projects
-            .into_iter()
-            .map(|(name, pdata)| (name.clone(), CachedProject{
-                _name: name, _data: pdata, ..Default::default()
-            }))
-            .collect();
-        let s = Self { _loader : loader, _projects };
-        Ok(s)
-    }
+    pub fn set_project_writer<Writer>(&mut self, writer: Writer)
+    where
+        Writer: project::Writer + 'static
+    { self.project_handler.set_writer(Box::new(writer)); }
 
-    pub fn load_projects(&mut self) {
-        for proj in self._projects.values_mut(){
-            proj.load_project(&self._loader);
-        } 
+    pub fn set_project_reader<Reader: project::Reader>(&mut self, reader: Reader)
+    where
+        Reader: project::Writer + 'static
+    { self.project_handler.set_reader(Box::new(reader)); }
 
-    }
 
-    pub fn get_loader(&self) -> &Loader{ &self._loader }
-    pub fn get_loader_mut(&mut self) -> &mut Loader { &mut self._loader }
+    pub fn set_manager_writer<Writer: manager::ManagerWriter>(&mut self, writer: Writer)
+    where
+        Writer: project::Writer + 'static
+    { self.manager_handler.set_writer(Box::new(writer)); }
 
-    pub fn load_project(&mut self, p : &mut CachedProject) {
-        p.load_project(&self._loader);
-    }
+    pub fn set_manager_reader<Reader: manager::ManagerReader>(&mut self, reader: Reader)
+    where
+        Reader: project::Writer + 'static
+    { self.manager_handler.set_reader(Box::new(reader)); }
 
-    #[allow(dead_code)]
-    fn drop_projects(&self){
-        todo!("drop projects");
-    }
-
-    #[allow(dead_code)]
-    fn drop_cache(&self){
-        todo!("drop cache");
-    }
-    pub fn get_projects_mut(&mut self) -> Vec<&mut CachedProject>{
-        self._projects
-            .values_mut()
-            .collect()
-    }
-
-    pub fn get_projects(&mut self) -> Vec<&CachedProject>{
-        self._projects
-            .values()
-            .collect()
-    }
-
-    pub fn new_project(&mut self, name: String, location: Location) -> Result<()>{
-        let existing_name = self._projects.iter().find(|(pname, _)| *pname == &name);
-        let existing_path = self._projects.iter().find(|(_, p)| &p._data.location == &location);
-
-        if existing_name.is_some() {
-            return Err(anyhow!("Project name {name} is already managed!"));
-        }
-        if existing_path.is_some() {
-            return Err(anyhow!("Project location {location} is already managed!"));
-        }
-
-        let c = CachedProject{
-            _name: name.clone(),
-            _data: ProjectData { location, ..Default::default() },
-            _loaded: true,
-            _proj: Some(Project{
-                project: ProjectInfo{
-                    name: name.clone(),
-                    ..Default::default()
-                },
-                ..Default::default()
-            }),
-            _cache: ProjectCache::default(),
-        };
-
-        self._projects.insert(name, c);
+    pub fn init(&mut self) -> Result<()>{
+        self.manager = self.manager_handler.read_manager()?;
+        self._inited = true;
         Ok(())
     }
 
-    pub fn commit_manager(&mut self) -> Result<()>{
-        let mut manager = Manager::default();
-        for (name, p) in self._projects.iter(){
-            manager.projects.insert(name.clone(), p._data.clone());
-        }
-
-        self._loader.write_manager(toml::to_string(&manager).unwrap())?;
-        Ok(())
+    pub fn load_projects(&mut self) -> Result<()>{
+        for (_, p) in &mut self.manager.projects{
+            self.project_handler.read(p)?;
+        } Ok(())
     }
 
-    pub fn commit_project(&mut self, name: &String) -> Result<()> {
-        let p = &self._projects[name];
-        if p._proj.is_none(){
-            return Err(anyhow!("Project is missing or it is not loaded!"))
-        }
-        let tml = toml::to_string(&p._proj.as_ref().unwrap())?;
-        self._loader.write_project(tml, &p._data.location)?;
-        Ok(())
-    }
-    pub fn init_project(&mut self, name: &String) -> Result<()> {
-        let p = self._projects.get_mut(name).unwrap();
-        p._proj = Some(Project::default());
-        self.commit_project(name)
+    pub fn commit_projects(&mut self) -> Result<()>{
+        for (_, p) in &mut self.manager.projects{
+            self.project_handler.write(p)?;
+        } Ok(())
+        
     }
 
-    pub fn commit_projects(&mut self) -> Result<()> {
-        let keys : Vec<_> = self._projects.keys().map(|f| f.clone()).collect();
-        for name in keys {
-            self.commit_project(&name)?;
-        }
-        Ok(())
+    pub fn get_project<S: AsRef<String>>(&mut self, name: S) -> Result<&mut Project> {
+        let project = self.manager.projects
+            .get_mut(name.as_ref())
+            .ok_or(anyhow!("Project Not found!"))?;
+        self.project_handler.read(project)?;
+        Ok(project)
     }
 
-    pub fn add_cached_project(&mut self, project: CachedProject) -> Result<()>{
-        if self._projects.contains_key(&project._name){
-            return Err(anyhow!("Project {} is already managed!", project._name));
-        }
-        if self._projects.iter().any(|(_, p)| &p._data.location == &project._data.location){
-            return Err(anyhow!("Project with location {} is already managed!", project._data.location));
-        }
-
-        self._projects.insert(project._name.clone(), project);
-        Ok(())
+    pub fn commit_project<S: AsRef<String>>(&mut self, name: S) -> Result<&mut Project> {
+        let project = self.manager.projects
+            .get_mut(name.as_ref())
+            .ok_or(anyhow!("Project Not found!"))?;
+        self.project_handler.write(project)?;
+        Ok(project)
     }
 
-    pub fn add_project(&mut self, name: String, location: Location) -> Result<()>{
-        if self._projects.contains_key(&name){
-            return Err(anyhow!("Project {name} is already managed!"));
-        }
-        if self._projects.iter().any(|(_, p)| &p._data.location == &location){
-            return Err(anyhow!("Project with location {location} is already managed!"));
-        }
-
-        let cached = CachedProject{
-            _name : name.clone(),
-            _data : ProjectData{
-                location, ..Default::default()
-            }, ..Default::default()
-        };
-        self._projects.insert(name, cached);
-        Ok(())
-    }
-
-    pub fn remove_project(&mut self, name: String) -> Result<()> {
-        self._projects.remove(&name).ok_or(anyhow!("project {name} not found"))?;
-        Ok(())
-    }
-
-    pub fn find_project_mut(&mut self, find_criteria: &FindCriteria) -> Result<&mut CachedProject>{
-        self._projects
-            .iter_mut()
-            .map(|(_,c)| c)
-            .find(|c| c.match_criteria(&find_criteria))
-            .ok_or(anyhow!("project with criteria ({find_criteria:#?}) was not found!"))
-    }
-
-    pub fn get_project(&self, find_criteria: &FindCriteria) -> Result<&CachedProject>{
-        self._projects
-            .iter()
-            .map(|(_,c)| c)
-            .find(|c| c.match_criteria(&find_criteria))
-            .ok_or(anyhow!("project with criteria ({find_criteria:#?}) was not found!"))
-    }
 }
