922302039da8e890983d6cf42929e4eee10317ab
Ulises Alanis <ulises.alanis.255@gmail.com>
2024-04-25 13:29:06 -0600

    going to change vectors to option vectors

--- crates/api/src/manager.rs
+++ crates/api/src/manager.rs
@@ -1,48 +1,44 @@
-use std::{collections::HashMap, rc::Rc};
+use std::collections::HashMap;
 
 use super::project::*;
 
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Manager{
-    pub projects: HashMap<String, Project>,
+    pub projects: HashMap<String, ProjectInfo>,
 }
 
-pub trait Writer{
+pub trait IO{
     fn write(&mut self, man: &Manager) -> Result<()>;
-}
-
-pub trait Reader{
     fn read(&self) -> Result<Manager>;
 }
 
 #[derive(Default)]
 pub struct ManagerHandler{
-    writer: Option<Rc<dyn Writer>>,
-    reader: Option<Rc<dyn Reader>>,
+    io: Option<Box<dyn IO>>,
 }
 
 impl std::fmt::Debug for ManagerHandler{
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result{
-        write!(f, "ManagerHandler{{ writer: {}, reader: {}, }}", self.writer.is_some(), self.reader.is_some())
+        write!(f, "ManagerHandler{{ writer: {} }}", self.io.is_some())
     }
 }
 
 impl ManagerHandler{
-    pub fn new() -> Self{ ManagerHandler{ writer: None, reader: None, } }
-    pub fn set_writer(&mut self, writer: Rc<dyn Writer>){ self.writer = Some(writer); }
-    pub fn set_reader(&mut self, reader: Rc<dyn Reader>){ self.reader = Some(reader); }
+    pub fn new() -> Self{ ManagerHandler{ io: None } }
+    pub fn set_io(&mut self, writer: Box<dyn IO>){ self.io = Some(writer); }
 
     pub fn write(&mut self, manager: &Manager) -> Result<()> {
-        match &mut self.writer{
+        match &mut self.io{
             Some(s) => s.write(&manager),
             None => return Err(anyhow::anyhow!("there is no manager writer")),
         }
     }
 
     pub fn read(&self) -> Result<Manager> {
-        match &self.reader {
+        match &self.io{
             Some(s) => s.read(),
             None => return Err(anyhow::anyhow!("there is no manager reader")),
         }
--- crates/client/src/cli/mod.rs
+++ crates/client/src/cli/mod.rs
@@ -1,12 +1,12 @@
 mod print;
-mod rename;
-mod git;
-mod features;
-mod delete;
-mod init;
-mod new;
-mod mark_feature;
-mod utils;
+// mod rename;
+// mod git;
+// mod features;
+// mod delete;
+// mod init;
+// mod new;
+// mod mark_feature;
+// mod utils;
 
 
 use std::path::PathBuf;
@@ -14,12 +14,13 @@ use std::path::PathBuf;
 use anyhow::{Result, anyhow};
 use clap::{Subcommand, Parser, Args};
 
-use print::PrintStruct;
-use init::InitStruct;
+use project_manager_api::Handler;
 
-use crate::{SystemHandler, SystemLoader};
+use crate::{ManagerTOML, ProjectTOML};
+
 use self::{
-    delete::DelStruct, features::AddFeat, git::GitStruct, mark_feature::MarkFeature, new::NewStruct
+    print::PrintStruct,
+//     delete::DelStruct, features::AddFeat, git::GitStruct, mark_feature::MarkFeature, new::NewStruct
 };
 
 #[derive(Parser, Debug)]
@@ -40,7 +41,7 @@ pub struct Arguments{
 struct NotDone;
 
 impl NotDone{
-    pub fn run(&self, _ : Arguments, _handler: SystemHandler) -> Result<()>{
+    pub fn run(&self, _ : Arguments, _handler: Handler) -> Result<()>{
         Err(anyhow!("not yet implemented"))
     }
 }
@@ -49,39 +50,48 @@ impl NotDone{
 enum Tree{
     // Daemon(DaemonStruct),
     Print(PrintStruct),
-    Init(InitStruct),
-    Delete(DelStruct),
-    New(NewStruct),
+    // Init(InitStruct),
+    // Delete(DelStruct),
+    // New(NewStruct),
 
-    SetParent(NotDone),
-    SetSubproject(NotDone),
+    // SetParent(NotDone),
+    // SetSubproject(NotDone),
+    // AddFeat(AddFeat),
+    // AddSubFeat(NotDone),
 
-    AddFeat(AddFeat),
-    AddSubFeat(NotDone),
+    // Tui(NotDone),
+    // DoneFeat(NotDone),
+    // Update(NotDone),
 
-    Tui(NotDone),
-    DoneFeat(NotDone),
-    Update(NotDone),
+    // MarkFeature(MarkFeature),
 
-    MarkFeature(MarkFeature),
+    // Git(GitStruct),
+}
 
-    Git(GitStruct),
+impl Arguments {
+    pub fn debug<S: AsRef<str>>(&self, info: &S){
+        let info = info.as_ref();
+        if self.debug{
+            println!("{}", info);
+        }
+    }
+    
 }
 
 pub fn cli() -> Result<()> {
     // set up stuff
     let args = Arguments::parse();
     if args.tree.is_none(){
-        return Ok(());
+        return Err(anyhow!("no arguments given!"));
     }
 
     let tree = args.tree.clone().unwrap();
 
-    let mut loader = SystemLoader::new();
-    loader.manager_path = if args.manager_path.is_some(){
-        if args.debug{
-            println!("setting path to: {:?}", args.manager_path.clone().unwrap());
-        }
+    let mut manager_toml = ManagerTOML::default();
+    manager_toml.path = if args.manager_path.is_some(){
+        args.debug(&
+            format!("setting path to: {:?}", args.manager_path.clone().unwrap())
+        );
         args.manager_path.clone().unwrap()
     }
     else{
@@ -91,21 +101,26 @@ pub fn cli() -> Result<()> {
         dir.set_extension("toml");
         dir
     };
-    if args.debug{
-        println!("handler path: {:?}", loader.manager_path);
-    }
-    let handler = SystemHandler::init(loader)?;
+
+    args.debug(&
+        format!("handler path: {:?}", manager_toml.path)
+    );
+    let mut handler = Handler::new();
+    handler.set_manager_io(manager_toml);
+    handler.set_project_io(ProjectTOML::default());
+    handler.init()?;
+    
 
     use Tree as TR;
 
     match tree{
         TR::Print(p) => p.run(args, handler)?,
-        TR::Init(i) => i.run(args, handler)?,
-        TR::Delete(d) => d.run(args, handler)?,
-        TR::New(n) => n.run(args, handler)?,
-        TR::AddFeat(f) => f.run(args, handler)?,
-        TR::Git(g) => g.run(args, handler)?,
-        TR::MarkFeature(f) => f.run(args, handler)?,
+        // TR::Init(i) => i.run(args, handler)?,
+        // TR::Delete(d) => d.run(args, handler)?,
+        // TR::New(n) => n.run(args, handler)?,
+        // TR::AddFeat(f) => f.run(args, handler)?,
+        // TR::Git(g) => g.run(args, handler)?,
+        // TR::MarkFeature(f) => f.run(args, handler)?,
         _ => NotDone::default().run(args, handler)?,
     }
 
