50101cf03102ce0330340da098100b5796940177
Ulises Alanis <ulises.alanis.255@gmail.com>
2024-10-05 20:10:53 -0600

    pull

--- crates/api/src/task.rs
+++ crates/api/src/task.rs
@@ -1,64 +1,25 @@
-<<<<<<< HEAD
 use ly::proc::builder;
 use serde::{Deserialize, Serialize};
 
 use crate::Description;
 use std::time::Duration;
 
-#[builder(Task)]
-=======
-use crate::*;
-
-#[allow(dead_code)]
-#[derive(Debug, Clone)]
-pub struct Task{
-    desc: Description,
-    done: bool,
-    min_time: time::Duration,
-    parent_task: Option<String>,
-    project: Option<String>,
-}
-
-impl Task {
-    pub const fn new(desc: Description) -> Self{
-        return Self{
-            desc, done: false,
-            min_time: Duration::from_secs(60 * 60),
-            parent_task: None,
-            project: None,
-        }
-    }
-    pub fn desc(mut self, desc: Description) -> Self { self.desc = desc; self }
-    pub fn done(mut self, done: bool) -> Self { self.done = done; self }
-    pub fn min_time(mut self, min_time: time::Duration) -> Self { self.min_time = min_time; self }
-    pub fn parent_task(mut self, parent_task: String) -> Self { self.parent_task = Some(parent_task); self }
-    pub fn project(mut self, parent_project: String) -> Self { self.project = Some(parent_project); self }
-}
-
->>>>>>> afbb097 (pull)
+#[builder(name = Task)]
 #[derive(Debug, Default, Clone)]
 pub struct TaskTable{
     pub(crate) desc: Description,
     pub(crate) done: bool,
 
-<<<<<<< HEAD
     // minimun time needed to perform the task min_time   : time::Duration,
     pub(crate) min_time: Duration,
-    #[builder(other_type: String)]
+    #[builder(ty = String)]
     pub(crate) parent_task: Option<usize>,
 
-    #[builder(other_type: String)]
+    #[builder(ty = String)]
     pub(crate) project    : Option<usize>,
 
-    #[builder(skip)]
+    #[builder_skip]
     pub(crate) id : usize,
-=======
-    // minimun time needed to perform the task
-    pub(crate) min_time   : time::Duration,
-
-    pub(crate) id : usize,
-    pub(crate) parent_task: Option<usize>,
-    pub(crate) project    : Option<usize>,
 }
 
 impl TaskTable {
@@ -74,5 +35,4 @@ impl TaskTable {
             id, parent_task, project,
         }
     }
->>>>>>> afbb097 (pull)
 }
