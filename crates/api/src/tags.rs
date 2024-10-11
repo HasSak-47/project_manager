f29e0f8d5ef780fa9c7e0d37f7abfe4de35658dc
arch machine <lilith.alanis.255@gmail.com>
2024-10-11 00:59:09 -0600

    indeed bad idea

--- crates/api/src/tags.rs
+++ crates/api/src/tags.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 #[builder(name = Tag, pass = derive(Debug, Default, Serialize, Deserialize))]
 #[derive(Debug, Default,)]
-struct TagTable{
+pub struct TagTable{
     tag: String,
     #[builder(skip)]
     id: usize,
