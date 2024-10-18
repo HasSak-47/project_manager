a2beda2c49c140a4ff3752419479ab4d5e893d4d
daniel-nix <ulises.alanis.255@gmail.com>
2024-10-18 12:06:25 -0600

    better tasks

--- crates/client/src/utils.rs
+++ crates/client/src/utils.rs
@@ -5,7 +5,7 @@ pub fn exists(db: &Database, path: PathBuf) -> bool {
     let path = Location::Path(path);
     return db.get_all_projects()
         .iter()
-        .find(|p| p.get_table().location == path)
+        .find(|p| *p.location() == path)
         .is_some();
 }
 
