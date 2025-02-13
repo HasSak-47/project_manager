d8391a08e284129e0dd47ccd51a2ceec1e167373
arch machine <lilith.alanis.255@gmail.com>
2025-02-13 10:57:00 -0600

    progress?

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -11,6 +11,7 @@ use ly::log::prelude::*;
 struct Event{
     id: usize,
     duration: u64,
+    priority: f64,
     date: u64,
 }
 
@@ -32,15 +33,33 @@ impl Task {
 }
 
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd)]
+enum EntryType{
+    #[default]
+    Task,
+    Event,
+}
+
+#[derive(Debug, Default, Clone, PartialEq, PartialOrd)]
 struct CollapsedEntry{
     id: usize,
     time: u64, // unix
+    e_type: EntryType,
+}
+
+impl CollapsedEntry{
+    fn new(id: usize, time: u64, e_type: EntryType) -> Self{
+        Self{ id, time, e_type }
+    }
+    fn new_event(id: usize, time: u64) -> Self{
+        Self{ id, time, e_type : EntryType::Event}
+    }
+    fn new_task(id: usize, time: u64) -> Self{
+        Self{ id, time, e_type : EntryType::Task}
+    }
 }
 
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd)]
 struct Scheduler{
-    current_time: u64,
-    delta_time: u64,
     tasks: Vec<Task>,
     events: Vec<Event>,
     schedule: Vec<CollapsedEntry>,
@@ -48,18 +67,43 @@ struct Scheduler{
 
 impl Scheduler{
     pub const fn new() -> Self{
-        Self { current_time: 0, delta_time: 0, tasks: Vec::new(), events: Vec::new(), schedule: Vec::new(), }
+        Self { tasks: Vec::new(), events: Vec::new(), schedule: Vec::new(), }
     }
 
     pub fn add_task(&mut self, task: Task) {
-        self.tasks.push(task);
+        let pos = match self.tasks.binary_search_by(|t| t.priority.total_cmp( &task.priority )){
+            Ok(k) => k,
+            Err(r) => r,
+        };
+        self.tasks.insert(pos, task);
     }
 
     pub fn add_event(&mut self, event: Event) {
+        let pos = match self.events.binary_search_by(|e| e.priority.total_cmp( &event.priority )){
+            Ok(k) => k,
+            Err(r) => r,
+        };
         self.events.push(event);
     }
 
-    pub fn slot(&mut self) {
+    /** returns true if the event was added false otherwise */
+    fn _add_event(&mut self, current_time: &mut u64, eventc: &mut Event, taskc: &mut Task) -> bool{
+        // if the event can even be started
+        if *current_time < eventc.date {
+            return false;
+        }
+        // if the task overlaps with the event skip to the event
+        // and the task is more important cancel event
+        if taskc.due_date < *current_time + eventc.duration && taskc.priority > eventc.priority {
+                return false;
+        }
+
+        *current_time += eventc.duration;
+        self.schedule.push(CollapsedEntry ::new_event(eventc.id, *current_time));
+        return true;
+    }
+
+    pub fn create_schedule(&mut self) {
     }
 }
 
@@ -78,6 +122,6 @@ fn main() -> Result<()> {
         s.add_task(task);
     }
 
-    s.slot();
+    s.create_schedule();
     Ok(())
 }
