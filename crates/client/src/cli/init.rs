1737a6451ae31a552ed0330422f74b0c7ec6b67b
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 22:55:56 -0600

    rework

--- crates/client/src/cli/init.rs
+++ crates/client/src/cli/init.rs
@@ -2,25 +2,26 @@ use std::{path::PathBuf, env::current_dir};
 
 use crate::SystemHandler;
 
-use super::{Params, Arguments};
+use super::Arguments;
 use clap::Args;
-use project_manager_api::{
-    config::manager::{Manager, ProjectData},
-    CachedProject,
-};
 
 use anyhow::Result;
+use project_manager_api::config::manager::Location;
 
 #[derive(Args, Debug, Clone)]
 pub struct InitStruct{
-    name: Option<String>,
+    name: String,
     path: Option<PathBuf>,
 }
 
 impl InitStruct{
-    pub fn run(self, args: Arguments, handler: SystemHandler) -> Result<()> {
-        let path = self.path.clone().unwrap_or(current_dir().unwrap());
+    pub fn run(self, _args: Arguments, mut handler: SystemHandler) -> Result<()> {
+        let mut path = self.path.unwrap_or(current_dir().unwrap());
+        path.push("status");
+        path.set_extension("toml");
 
+        handler.add_project(self.name, Location::path(path));
+        handler.commit_manager()?;
         Ok(())
     }
 }
