a49cae0cdbced735120220b22804e908dac2ddd1
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 22:55:57 -0600

    upgraded toml

--- crates/client/src/cli/new.rs
+++ crates/client/src/cli/new.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, env::current_dir};
+use std::{env::current_dir, fs::File, io::Write, path::PathBuf};
 
 use super::Arguments;
 use clap::Args;
@@ -7,6 +7,7 @@ use anyhow::Result;
 use project_manager_api::{desc::Descriptor, project::Project, Database, Location};
 
 use crate::VERSION;
+use ly::log::prelude::*;
 
 #[derive(Args, Debug, Clone)]
 #[clap(about = include_str!("abouts/NewStruct.txt").trim_end())]
@@ -23,14 +24,36 @@ pub struct NewStruct{
 impl NewStruct{
     pub fn run(self, _args: Arguments, mut db: Database) -> Result<()> {
         let path = self.path.unwrap_or(current_dir().unwrap());
+
+        let _ = log!("creating project at {}", path.display());
+
+        let mut status_path = path.clone();
+        status_path.push("status");
+        status_path.set_extension("toml");
+
+        let _ = log!("with status at {}", status_path.display());
+
+
         let p = Project::new()
             .desc(Descriptor::new()
                   .name(self.name)
-                  .version(self.version))
+                  .version(self.version)
+                  .edition(self.edition)
+            )
             .location(Location::Path(path));
+        let _ = log!("created project {p:?}");
+
+        let mut file = File::create(&status_path)?;
+        let _ = log!("created status file");
+        let s = toml::to_string(&p).unwrap();
+        let _ = log!("parsed project data");
+
+        file.write(s.as_bytes())?;
+
+        db.add_full_project(p)?;
+        db.write_data()?;
 
 
-        db.new_project(p)?;
 
         Ok(())
     }
