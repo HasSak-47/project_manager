01f0b832b50c27cdf44d981b8199767aa0a945ca
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 22:55:57 -0600

    got distracted :p

--- crates/api/src/tags.rs
+++ crates/api/src/tags.rs
@@ -2,7 +2,7 @@ use ly::proc::builder;
 use serde::{Deserialize, Serialize};
 
 
-#[builder(name = Tag, pass = derive(Debug, Default, Serialize, Deserialize))]
+#[builder(name = Tag, pass = derive(Debug, Default, Serialize, Deserialize, Clone))]
 #[derive(Debug, Default,)]
 pub struct TagTable{
     tag: String,
