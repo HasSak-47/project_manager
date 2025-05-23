e195796d518804b1a2e2dd7014a8e91178c5c094
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 22:55:57 -0600

    task client is gonna be bad

--- crates/client/src/cli/features.rs
+++ crates/client/src/cli/features.rs
@@ -1,9 +1,11 @@
 
+use std::env::current_dir;
+
 use crate::utils::*;
 use clap::Args;
 
 use anyhow::{anyhow, Result};
-use project_manager_api::{desc::Descriptor, task::Task, Database};
+use project_manager_api::{desc::Descriptor, task::Task, Database, DatabaseError, Location};
 
 use super::Arguments;
 
@@ -15,22 +17,33 @@ pub struct AddFeat{
 
     #[clap(short, default_value = "todo")]
     r#type : String,
+
+    #[clap(long)]
+    orphan: bool,
 }
 
 impl AddFeat{
     pub fn run(self, _params: Arguments, mut db: Database) -> Result<()> {
-        let projects = db.get_all_projects();
-        let project = projects.iter()
-            .find(|p| p.get_table().desc.name == self.name)
-            .ok_or(anyhow!("could not find project"))?
-            .get_table();
-
+        let cwd = Location::Path(current_dir()?);
+        let project = if !self.orphan{
+            db.get_all_projects()
+                .iter()
+                .find(|p| *p.location() == cwd)
+                .ok_or(anyhow!("project not found"))
+                .and_then(|p| Ok(p.name()))?
+                .clone()
+        }else{
+             "".to_string()
+        };
         let task = Task::new()
             .desc(Descriptor::new()
                 .name(self.name)
                 .priority(self.priority)
                 .difficulty(self.difficulty)
-            ).project(project.desc.name.clone());
+            ).project(project);
+
+        db.add_full_task(task)?;
+        db.write_data()?;
 
         Ok(())
     }
