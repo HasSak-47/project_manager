4d726e9bfcbc1cd8264130c3b5c66fd760bd3374
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 22:55:56 -0600

    fucker

--- src/update/v0_1_0.rs
+++ src/update/v0_1_0.rs
@@ -1,29 +1,6 @@
 use serde::{Deserialize, Serialize};
 use toml::{Table, value::Array};
 
-use crate::config::project::{Feature, Project};
-
-mod prev_data{
-use serde::{Deserialize, Serialize};
-use toml::{Table, value::Array};
-
-#[derive(Deserialize, Serialize, Debug, Default, Clone)]
-pub struct Feature{
-    pub name       : String,
-    pub priority   : f32,
-    pub difficulty : f32,
-    pub description: Option<String>,
-    pub sub_feature: Vec<Feature>,
-}
-
-#[derive(Debug, Default, Clone)]
-pub struct Project{
-    pub info : ProjectInfo,
-    pub subproj: Vec<Project>,
-    pub todo: Vec<Feature>,
-    pub done: Vec<Feature>,
-}
-
 #[derive(Default, Clone, Deserialize, Serialize, Debug)]
 pub struct ProjectInfo{
     pub name: String,
@@ -39,13 +16,22 @@ pub struct ProjectToml{
     todo    : Option<Array>, 
     done    : Option<Array>, 
 }
+
+#[derive(Deserialize, Serialize, Debug, Default, Clone)]
+pub struct FeatureV0_1_0{
+    pub name       : String,
+    pub priority   : f32,
+    pub difficulty : f32,
+    pub description: Option<String>,
+    pub done: Vec<FeatureV0_1_0>,
+    pub todo: Vec<FeatureV0_1_0>,
 }
 
-impl From<prev_data::Project> for Project{
-    fn from(value: prev_data::Project) -> Self {
-        let mut p = Project::default();
-
-
-        p
-    }
+#[derive(Deserialize, Serialize, Debug, Default, Clone)]
+pub struct FeatureV0_0_0{
+    pub name       : String,
+    pub priority   : f32,
+    pub difficulty : f32,
+    pub description: Option<String>,
+    pub subfeature : Vec<FeatureV0_0_0>,
 }
