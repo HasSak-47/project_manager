2e8b9e8e4ec4770137b9ac18d0f7b33ece5df287
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 22:55:57 -0600

    merge

--- crates/api/src/project.rs
+++ crates/api/src/project.rs
@@ -1,3 +1,57 @@
 use super::Location;
 use serde::{Deserialize, Serialize};
+use crate::*;
 
+#[derive(Debug, Default, Clone)]
+pub struct Project{
+    desc: Description,
+    last_worked: Option<time::Instant>,
+    location: Option<Location>,
+    parent: Option<String>,
+}
+
+impl Project {
+    pub const fn new(desc: Description) -> Self{
+        Self {desc, last_worked: None, location: None, parent: None}
+    }
+    pub fn last_worked(mut self, last_worked : time::Instant) -> Self{ self.last_worked = Some(last_worked); self }
+    pub fn location(mut self, location : Location) -> Self{ self.location = Some(location); self }
+    pub fn parent(mut self, parent : String) -> Self{ self.parent = Some(parent); self }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct ProjectTable{
+    pub(crate) desc: Description,
+    pub(crate) last_worked: Option<time::Instant>,
+    pub(crate) location: Option<Location>,
+
+    pub(crate) id    : usize,
+    pub(crate) parent: Option<usize>,
+}
+
+impl ProjectTable {
+    pub fn from_project(project: Project, pool: &Pool) -> Self{
+        let id = pool.projects.last().and_then(|s| Some(s.id)).unwrap_or(pool.projects.len());
+        let parent = project.parent.and_then( |p| pool.search_project_id(p).ok()) ;
+        return ProjectTable{
+            desc: project.desc,
+            location: project.location,
+            last_worked: project.last_worked,
+            parent, id,
+        };
+    }
+
+    pub fn from_project_result(project: Project, pool: &Pool) -> Result<Self, PoolError>{
+        let id = pool.projects.last().and_then(|s| Some(s.id)).unwrap_or(pool.projects.len());
+        let parent = if let Some(parent) = project.parent {
+            Some(pool.search_project_id(parent)?)
+        }
+        else { None };
+        return Ok(ProjectTable{
+            desc: project.desc,
+            location: project.location,
+            last_worked: project.last_worked,
+            parent, id,
+        });
+    }
+}
--- crates/api/src/task.rs
+++ crates/api/src/task.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 use ly::proc::builder;
 use serde::{Deserialize, Serialize};
 
@@ -5,11 +6,42 @@ use crate::Description;
 use std::time::Duration;
 
 #[builder(Task)]
+=======
+use crate::*;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Task{
+    desc: Description,
+    done: bool,
+    min_time: time::Duration,
+    parent_task: Option<String>,
+    project: Option<String>,
+}
+
+impl Task {
+    pub const fn new(desc: Description) -> Self{
+        return Self{
+            desc, done: false,
+            min_time: Duration::from_secs(60 * 60),
+            parent_task: None,
+            project: None,
+        }
+    }
+    pub fn desc(mut self, desc: Description) -> Self { self.desc = desc; self }
+    pub fn done(mut self, done: bool) -> Self { self.done = done; self }
+    pub fn min_time(mut self, min_time: time::Duration) -> Self { self.min_time = min_time; self }
+    pub fn parent_task(mut self, parent_task: String) -> Self { self.parent_task = Some(parent_task); self }
+    pub fn project(mut self, parent_project: String) -> Self { self.project = Some(parent_project); self }
+}
+
+>>>>>>> afbb097 (pull)
 #[derive(Debug, Default, Clone)]
 pub struct TaskTable{
     pub(crate) desc: Description,
     pub(crate) done: bool,
 
+<<<<<<< HEAD
     // minimun time needed to perform the task min_time   : time::Duration,
     pub(crate) min_time: Duration,
     #[builder(other_type: String)]
@@ -20,4 +52,27 @@ pub struct TaskTable{
 
     #[builder(skip)]
     pub(crate) id : usize,
+=======
+    // minimun time needed to perform the task
+    pub(crate) min_time   : time::Duration,
+
+    pub(crate) id : usize,
+    pub(crate) parent_task: Option<usize>,
+    pub(crate) project    : Option<usize>,
+}
+
+impl TaskTable {
+    pub(crate) fn from_task(task: Task, pool: &Pool)-> Self{
+        let id = pool.tasks.last().and_then(|s| Some(s.id)).unwrap_or(pool.tasks.len());
+        let parent_task = task.parent_task.and_then(|p| pool.search_task_id(p).ok() );
+        let project = task.project.and_then(|p| pool.search_project_id(p).ok() );
+
+        return Self{
+            desc: task.desc,
+            done: task.done,
+            min_time: task.min_time,
+            id, parent_task, project,
+        }
+    }
+>>>>>>> afbb097 (pull)
 }
