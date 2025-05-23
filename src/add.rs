31007db7bfe314556c07a1e59db98357f2cff161
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 22:55:56 -0600

    idk man

--- src/add.rs
+++ src/add.rs
@@ -1,3 +1,35 @@
 use super::config::*;
+use crate::errors::Result;
+use toml::{Table, Value};
 
-pub fn add() {}
+use std::env;
+use std::io;
+
+fn catcher() -> Result<()>{
+    let mut manager = manager::load_manager();
+
+    let current_dir = env::current_dir()?.to_str().unwrap().to_string();
+    println!("set name for ({current_dir})");
+
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).unwrap();
+
+    let mut projects = match &manager.projects{
+        Some(n) => n.clone(),
+        None    => Table::new(),
+    };
+
+    projects.insert(name, Value::String(current_dir));
+    manager.projects = Some(projects);
+
+    let path = manager::get_config_path();
+
+    let data : String = toml::to_string(&manager).unwrap();
+    std::fs::write(path, data.as_bytes())?;
+
+    Ok(())
+}
+
+pub fn add(){
+    catcher().unwrap();
+}
--- src/daemon.rs
+++ src/daemon.rs
@@ -1,3 +1,10 @@
-use super::config::*;
+use std::io::{self, prelude};
 
-pub fn daemon() {}
+pub fn daemon(){
+}
+
+pub fn init_daemon() {
+}
+
+pub fn update_daemon(){
+}
