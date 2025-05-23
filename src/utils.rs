443a1edb61b2f8cb2ef49c53d25cb806718e3c49
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 22:55:56 -0600

    stuff

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use crate::ProjectResult;
 use std::path::Path;
 
 pub fn read_file<S: AsRef<Path>>(path: S) -> ProjectResult<String>{
-    let file = File::open(path).unwrap();
+    let file = File::open(path)?;
     let mut buf_reader = BufReader::new(file);
     let mut data = Vec::new();
     buf_reader.read_to_end(&mut data)?;
