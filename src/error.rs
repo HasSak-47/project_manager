8e65481b1c6c73a5b417cc03f7e67b192c3517ea
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-05-22 22:55:56 -0600

    idk man

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,11 @@
+#[allow(dead_code)]
 #[derive(Debug)]
 pub enum ProjectError{
     #[allow(dead_code)]
     Option,
     DirNotFound,
     DirToStr,
+    CliOptionUnknown,
     IOError(std::io::Error),
     UTF8Error(std::str::Utf8Error),
     UTF8ErrorString(std::string::FromUtf8Error),
@@ -28,25 +30,3 @@ into_error!(std::str::Utf8Error, UTF8Error);
 into_error!(std::string::FromUtf8Error, UTF8ErrorString);
 into_error!(toml::de::Error, TOMLFromStr);
 into_error!(toml::ser::Error, TOMLToStr);
-
-pub fn get_dir(a: fn() -> Option<std::path::PathBuf>) -> ProjectResult<String>{
-    use ProjectError as PE;
-    let str = a().ok_or(PE::DirNotFound)?.to_str().ok_or(PE::DirToStr)?.to_string();
-    Ok(str)
-}
-
-#[allow(dead_code)]
-pub fn to_res<T>(o: Option<T>) -> ProjectResult<T>{
-    match o{
-        Some(s) => Ok(s),
-        None => Err(ProjectError::Option)
-    }
-}
-
-#[allow(dead_code)]
-pub fn to_res_err<T>(o: Option<T>, e: ProjectError) -> ProjectResult<T>{
-    match o{
-        Some(s) => Ok(s),
-        None => Err(e)
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,26 @@
-#![allow(unused_imports)]
-
 mod cli;
 mod update;
 mod config;
 mod error;
 mod utils;
-mod baked;
 
-use config::project::{Feature, Project, ProjectToml};
-use error::*;
-use config::{manager::Manager, project};
+use cli::run_cli;
+use error::ProjectResult;
+use config::manager::Manager;
 use std::env::args;
 
 #[allow(dead_code)]
 const EDITION : &str = "0.1.0";
 
 fn main() -> ProjectResult<()>{
-    /*
-    let mut args : Vec<String> = args().into_iter().collect();
+    Manager::create_data()?;
+    let mut args : Vec<_> = args().collect();
     args.remove(0);
-    let opt = if args.len() == 0 {
-        String::from("list")
-    } else { args[0].clone() };
-    if opt == "update"{
-        UpdateStatusEdition::run()?;
-    }
-    else
-    if opt == "random"{
-        SelectRandomProject::run()?;
+    if args[0] == "tui"{
+        
     }
     else{
-        PrintPercentajes::run()?;
+        run_cli(args)?;
     }
-    */
     Ok(())
 }
